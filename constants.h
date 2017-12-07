da93ddc6d26c3ca3ba87646bd3eab7d4f273b23e
Austin <tuttl059@umn.edu>
2017-12-07 10:18:00 -0600

    Update CMake
    Update the CMakeLists file to compile with the updated petsc library.

--- constants.h
+++ constants.h
@@ -8,7 +8,7 @@
 
 static const int use_en_deriv = 0; //if true, will use the derivative of the electroneutrality condition for the system of equations
 static const int use_direct_solve = 0; //if true, will use gmres instead of direct solve
-static const int details = 0; //if true, will show how many iterations were necessary for each newton solve, and the residual
+static const int details = 1; //if true, will show how many iterations were necessary for each newton solve, and the residual
 static const int krecordfreq = 10; //determines how many time steps to run before recording the state variables
 static const int two_points_exct = 0;   //if true, triggers SD at origin and (Nx/2,1) (halfway along x-axis)
 static const int savefreq = 500;
@@ -21,8 +21,8 @@ static const   PetscReal D[3] = {1.33e-5, 1.96e-5, 2.03e-5};      //diffusion co
 //grid parameters
 // static const   PetscReal dt = 1e-2 ;        //time step (in s)
 static const 	PetscReal dt = 0.01;
-//static const   PetscReal Time = 3e-2;
- static const   PetscReal Time = 1e-1;
+static const   PetscReal Time = 3e-2;
+// static const   PetscReal Time = 1e-1;
 // static const  PetscReal   Time=10;
 //static const    Time = 60//2e-2        //total simulated time in seconds
 // static const    Time=2e-2
